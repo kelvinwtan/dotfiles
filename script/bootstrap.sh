3d62a6e2d2d1e72e9c432a0bdd53ab43000b4899
kelvinwtan <kelvin@eucalyptus.vc>
2021-01-22 22:53:54 +1100

    update

---
+++ script/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+brew_add() { if brew ls --versions "$1"; then HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade "$1"; else HOMEBREW_NO_AUTO_UPDATE=1 brew install "$1"; fi }
+
+brew_add coreutils
+brew_add git
+brew_add golang
+brew_add grep
+brew_add vim
+brew_add wget
+brew_add zsh
+
+brew tap homebrew/cask
+
+CASKS=(
+  starship
+  visual-studio-code
+)
+
+brew cask install ${CASKS[@]}
+
+brew cleanup

15c5ec50a7425fd918c805fb66a265c7ecac1f33
kelvinwtan <hello@kelvintan.io>
2021-01-23 20:37:53 +1100

    add file

---
+++ zsh/config.zsh
@@ -0,0 +1,46 @@
+#!/bin/zsh
+export LSCOLORS='Gxfxcxdxbxegedabagacad'
+export CLICOLOR=true
+
+HISTFILE=~/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+
+# share history between sessions ???
+setopt SHARE_HISTORY
+# add timestamps to history
+setopt EXTENDED_HISTORY
+setopt PROMPT_SUBST
+setopt COMPLETE_IN_WORD
+# adds history
+setopt APPEND_HISTORY
+# adds history incrementally and share it across sessions
+setopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY
+# don't record dupes in history
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_REDUCE_BLANKS
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_VERIFY
+setopt HIST_EXPIRE_DUPS_FIRST
+# dont ask for confirmation in rm globs*
+setopt RM_STAR_SILENT
+
+# backward and forward word with option+left/right
+bindkey '^[^[[D' backward-word
+bindkey '^[b' backward-word
+bindkey '^[^[[C' forward-word
+bindkey '^[f' forward-word
+
+# to to the beggining/end of line with fn+left/right or home/end
+bindkey "${terminfo[khome]}" beginning-of-line
+bindkey '^[[H' beginning-of-line
+bindkey "${terminfo[kend]}" end-of-line
+bindkey '^[[F' end-of-line
+
+# delete char with backspaces and delete
+bindkey '^[[3~' delete-char
+
+# delete word with ctrl+backspace
+bindkey '^[[3;5~' backward-delete-word
